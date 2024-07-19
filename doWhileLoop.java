ec5322121d0f11d21310c51253fde04dc7d0faf0
Akash Shrestha <41756657+akashstha@users.noreply.github.com>
2024-07-18 22:37:19 -0500

    [doWhileLoop]adding the file for doWhile loop

---
+++ doWhileLoop.java
@@ -0,0 +1,10 @@
+public class doWhileLoop{
+    public static void main(String a[] ) {
+        int dowhileLoop_Var = 5;
+         do{
+            System.out.println("Print the do while loop variable at least once");
+            dowhileLoop_Var++;
+         }while(dowhileLoop_Var <=4);
+
+    }
+}
