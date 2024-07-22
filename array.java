69bf90b4a4f2589dcad287c616ad347078ea846e
Akash Shrestha <41756657+akashstha@users.noreply.github.com>
2024-07-22 16:40:50 -0500

    [Object]adding array concept  and adding it in a class

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array{
+    public static void main(String[] args) {
+        int arr[]=new int[4];
+        arr[0] = 2;
+        arr[3] = 123;
+      for(int i = 0; i<=arr.length;i++){
+        System.out.println(arr[i]);
+
+      }  
+}
+}
