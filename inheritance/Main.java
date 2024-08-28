2afd2d2be6fe12e671a32bd1c2be824ec37ceef4
Akash Shrestha <41756657+akashstha@users.noreply.github.com>
2024-08-28 16:25:22 -0500

    [JavaCore]adding the example and explanation of Multilevel  Inheritance

--- inheritance/Main.java
+++ inheritance/Main.java
@@ -2,16 +2,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        AdvanceCalculator obj = new AdvanceCalculator();
+        TopLevelCalculator obj = new TopLevelCalculator();
         int r1 = obj.addition(2,2);
         int r2 = obj.subtraction(6,2);
         int r3 = obj.multiplication(2,2);
         int r4 = obj.division(20,2);
+        double r5 = obj.power(20,2);
 
         System.out.println("Addition:" + r1);
         System.out.println("subtraction:" + r2);
         System.out.println("Muliplication:" + r3);
         System.out.println("Division:" + r4);
+        System.out.println("Power:" + r5);
 
     }
 }
